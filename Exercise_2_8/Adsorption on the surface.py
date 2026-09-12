ea29e5f6a4a341e93839763c3f2ae04057a943a4
raniajawabreh985-a11y <raniajawabreh985@gmail.com>
2026-09-12 19:42:31 +0300

    Update Adsorption on the surface.py

--- Exercise_2_8/Adsorption on the surface.py
+++ Exercise_2_8/Adsorption on the surface.py
@@ -1,113 +1,53 @@
 #!/usr/bin/env python3
-from ase.io import read, write
-from ase.calculators.espresso import Espresso
-
-atoms = read('h_c8.vasp')
-
-pseudopotentials = {
-    'C': 'C.pbe-n-kjpaw_psl.1.0.0.UPF',
-    'H': 'H.pbe-rrkjus_psl.1.0.0.UPF'
-}
-
-input_data = {
-    'control': {
-        'calculation': 'relax',
-        'restart_mode': 'from_scratch',
-        'pseudo_dir': './',
-    },
-    'system': {
-        'ecutwfc': 30,
-        'occupations': 'smearing',
-        'smearing': 'mv',
-        'degauss': 0.01,
-    },
-    'electrons': {
-        'conv_thr': 1.0e-6,
-    },
-}
-
-calc = Espresso(
-    input_data=input_data,
-    pseudopotentials=pseudopotentials,
-    kpts=(2, 2, 1)
-)
-
-atoms.calc = calc
-energy = atoms.get_potential_energy()
-write('final_relaxed_structure.vasp', atoms)
-print(f"Total Energy: {energy} eV")
-
-#!/usr/bin/env python3
 from ase.io import read
-from ase.calculators.espresso import Espresso
+from ase.calculators.espresso import Espresso, EspressoProfile
 
-atoms = read('h.vasp')
+profile = EspressoProfile(command='pw.x', pseudo_dir='.')
 
-pseudopotentials = {
-    'H': 'H.pbe-rrkjus_psl.1.0.0.UPF'
-}
-
-input_data = {
-    'control': {
-        'calculation': 'scf',
-        'restart_mode': 'from_scratch',
-        'pseudo_dir': './',
+# 1. Total energy of pure C8 slab
+c8 = read('c8.vasp')
+c8.calc = Espresso(
+    profile=profile,
+    input_data={
+        'control': {'calculation': 'scf', 'prefix': 'c8_scf', 'outdir': './tmp'},
+        'system': {'ecutwfc': 30.0, 'occupations': 'smearing', 'smearing': 'gaussian', 'degauss': 0.01},
+        'electrons': {'conv_thr': 1e-6}
     },
-    'system': {
-        'ecutwfc': 30,
-        'occupations': 'smearing',
-        'smearing': 'mv',
-        'degauss': 0.01,
-        'nspin': 2,
-    },
-    'electrons': {
-        'conv_thr': 1.0e-6,
-    },
-}
-
-calc = Espresso(
-    input_data=input_data,
-    pseudopotentials=pseudopotentials,
-    kpts=(1, 1, 1)
+    pseudopotentials={'C': 'C.upf'}
 )
+e_c8 = c8.get_total_energy()
 
-atoms.calc = calc
-energy = atoms.get_potential_energy()
-print(f"Total energy: {energy} eV")
-
-#!/usr/bin/env python3
-from ase.io import read
-from ase.calculators.espresso import Espresso
-
-atoms = read('c.vasp')
-
-pseudopotentials = {
-    'C': 'C.pbe-n-kjpaw_psl.1.0.0.UPF'
-}
-
-input_data = {
-    'control': {
-        'calculation': 'scf',
-        'restart_mode': 'from_scratch',
-        'pseudo_dir': './',
+# 2. Total energy of isolated H atom
+h = read('h.vasp')
+h.calc = Espresso(
+    profile=profile,
+    input_data={
+        'control': {'calculation': 'scf', 'prefix': 'h_scf', 'outdir': './tmp'},
+        'system': {'ecutwfc': 30.0, 'occupations': 'smearing', 'smearing': 'gaussian', 'degauss': 0.01},
+        'electrons': {'conv_thr': 1e-6}
     },
-    'system': {
-        'ecutwfc': 30,
-        'occupations': 'smearing',
-        'smearing': 'mv',
-        'degauss': 0.01,
-    },
-    'electrons': {
-        'conv_thr': 1.0e-6,
-    },
-}
-
-calc = Espresso(
-    input_data=input_data,
-    pseudopotentials=pseudopotentials,
-    kpts=(2, 2, 1)
+    pseudopotentials={'H': 'H.upf'}
 )
+e_h = h.get_total_energy()
 
-atoms.calc = calc
-energy = atoms.get_potential_energy()
-print(f"Total energy: {energy} eV")
+# 3. Total energy of H adsorbed on C8
+h_c8 = read('h_c8.vasp')
+h_c8.calc = Espresso(
+    profile=profile,
+    input_data={
+        'control': {'calculation': 'scf', 'prefix': 'hc8_scf', 'outdir': './tmp'},
+        'system': {'ecutwfc': 30.0, 'occupations': 'smearing', 'smearing': 'gaussian', 'degauss': 0.01},
+        'electrons': {'conv_thr': 1e-6}
+    },
+    pseudopotentials={'C': 'C.upf', 'H': 'H.upf'}
+)
+e_h_c8 = h_c8.get_total_energy()
+
+# 4. Binding energy calculation
+e_binding = e_c8 + e_h - e_h_c8
+
+print(f"--- Adsorption Results ---")
+print(f"E(C8):      {e_c8:.6f} eV")
+print(f"E(H):       {e_h:.6f} eV")
+print(f"E(H@C8):    {e_h_c8:.6f} eV")
+print(f"E_binding:  {e_binding:.6f} eV")
